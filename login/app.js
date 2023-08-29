bdddd1378be1df5050c1c37dbfea77f043d69ff1
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-29 13:38:41 -0300

    primeira coisa

---
+++ login/app.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD_PGcs3bRoVIok8Dpc-PL5IRl7LyVo-1c",
+    authDomain: "projeto2miateste-986fa.firebaseapp.com",
+    projectId: "projeto2miateste-986fa",
+    storageBucket: "projeto2miateste-986fa.appspot.com",
+    messagingSenderId: "422205279897",
+    appId: "1:422205279897:web:b24b38e4a530f7c2947208"
+  };
+  firebase.initializeApp(firebaseConfig);
